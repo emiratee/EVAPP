df1242294b6ae6f7a9bc80efa8a969a9526e9bd0
erik <erik@lenzmann.net>
2023-11-25 14:18:57 +0100

    plain card done

--- Client/components/TripCard/TripCard.tsx
+++ Client/components/TripCard/TripCard.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import React from 'react';
 
 const trips = [
@@ -7,8 +8,29 @@ const trips = [
         destination_city: 'Hamburg',
         departure_time: '12:15',
         destination_time: '15:30',
+        stops: [
+            {
+                city: 'Lüneburg',
+                arrival_time: '14:45'
+            }
+        ],
         driver: {
-            name: 'Manfred',
+            name: 'Vladislav',
+            rating: '4,7'
+        },
+        price: '18.00',
+        seats: {
+            available: 2,
+            total: 3
+        }
+    },
+    {
+        departure_city: 'Berlin',
+        destination_city: 'Recklinghausen',
+        departure_time: '12:15',
+        destination_time: '16:35',
+        driver: {
+            name: 'Vladislav',
             rating: '4,7'
         },
         price: '18.00',
@@ -23,22 +45,7 @@ const trips = [
         departure_time: '12:15',
         destination_time: '15:30',
         driver: {
-            name: 'Manfred',
-            rating: '4,7'
-        },
-        price: '18.00',
-        seats: {
-            available: 2,
-            total: 3
-        }
-    },
-    {
-        departure_city: 'Berlin',
-        destination_city: 'Hamburg',
-        departure_time: '12:15',
-        destination_time: '15:30',
-        driver: {
-            name: 'Manfred',
+            name: 'Vladislav',
             rating: '4,7'
         },
         price: '18.00',
@@ -60,7 +67,7 @@ const TripCardItem = ({ trip }) => {
 
                             <View style={styles.line}></View>
                         </View>
-                        <View style={styles.textInfo}>
+                        <View>
                             <Text style={styles.city}>{trip.departure_city}</Text>
                             <Text style={styles.time}>{trip.departure_time}</Text>
                         </View>
@@ -68,14 +75,29 @@ const TripCardItem = ({ trip }) => {
                     {/* Destination Information */}
                     <View style={styles.routeItem}>
                         <View style={styles.dot}></View>
-                        <View style={styles.textInfo}>
+                        <View>
                             <Text style={styles.city}>{trip.destination_city}</Text>
                             <Text style={styles.time}>{trip.destination_time}</Text>
                         </View>
                     </View>
                 </View>
-                <View>
-                    {/* Other Details */}
+                <View style={styles.information}>
+                    <View style={styles.priceContainer}>
+                        <Text style={styles.price}>{`${trip.price}€`}</Text>
+                    </View>
+                    <View style={styles.informationItem}>
+                        <View>
+                            <Text style={styles.name}>{trip.driver.name}</Text>
+                            <View style={styles.ratingContainer}>
+                                <MaterialCommunityIcons name='star' size={15} />
+                                <Text style={styles.rating}>{trip.driver.rating}</Text>
+                            </View>
+                        </View>
+                        <Image
+                            source={require('../../assets/images/driver.png')}
+                            style={{ height: 50, width: 50, borderRadius: 50 }}
+                        />
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
@@ -116,6 +138,7 @@ const styles = StyleSheet.create({
     },
     cardContainer: {
         flex: 1,
+        flexDirection: 'row',
         marginVertical: 5,
         backgroundColor: '#fff',
         borderColor: '#000',
@@ -124,10 +147,12 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     city: {
-        fontSize: 18
+        fontSize: 18,
+        fontWeight: 'bold'
     },
     time: {
-        fontSize: 13
+        fontSize: 13,
+        fontWeight: '200'
     },
     route: {
         flex: 1,
@@ -153,11 +178,46 @@ const styles = StyleSheet.create({
         backgroundColor: '#000',
         width: 6,
         height: 100,
-        top: 10, // Adjust this value according to your layout
-        bottom: 0, // Adjust this value according to your layout
-        left: 3, // Adjust this value according to your layout
+        top: 10,
+        bottom: 0,
+        left: 3,
     },
-    textInfo: {
+    priceContainer: {
         flex: 1,
+        backgroundColor: '#000',
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 12,
+        maxHeight: 40,
+        justifyContent: 'center',
+        paddingHorizontal: 10,
     },
+    price: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    information: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        gap: 10,
+    },
+    informationItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
+    },
+    name: {
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    ratingContainer: {
+        flexDirection: 'row',
+        gap: 3
+    },
+    rating: {
+        fontSize: 12
+    }
 });
