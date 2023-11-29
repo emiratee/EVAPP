800bfdc2e9b9818e937009e5659ba2c33c95cd5b
erik <erik@lenzmann.net>
2023-11-29 10:30:44 +0100

    feat: reworkes result card

--- Client/app/(tabs)/addCredits.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from '../../components/Themed';
-
-export default function addCredits() {
-    return (
-        <Text>Add more credits - Payment</Text>
-    );
-}
--- Client/app/(tabs)/t3.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text } from '../../components/Themed';
-import TripCard from '../../components/TripCard/TripCard';
-
-export default function t3() {
-    return (
-        <Text>Messages</Text>
-    );
-}
