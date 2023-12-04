eff217b5860e901f02335ccdae9c2a4c0b601a0b
PilySwatch <pilar.cuellarta@gmail.com>
2023-12-04 17:06:07 +0100

    feat: add chat functionality on the FE & install Socket.IO

--- Client/components/Chat/ChatMessage.tsx
+++ Client/components/Chat/ChatMessage.tsx
@@ -3,36 +3,49 @@ import React from 'react';
 
 
 
-const ChatMessage = () => {
+const ChatMessage = ({ text, time }) => {
   return (
-    <View>
-      <Text style={{color: '#fff', textAlign: 'center', fontSize: 52}}>ChatMessage</Text>
-      <View style={styles.container}>
-        <Text>Hallihallo! wo bist du? Ich warte auf dich in Stresemannstr. 123C</Text>
-        <Text>time</Text>
+    <>
+    {/* <View style={[styles.container, {justifyContent: 'flex-start'}]}>
+      <View style={[styles.message, {borderBottomLeftRadius: 0, borderBottomRightRadius: 10, backgroundColor:'#9f81e3'}]}>
+        <Text>{text}</Text>
+        
+        <Text style={[styles.time, {textAlign: 'left', paddingRight: 0, paddingLeft: 10}]}>{time}</Text>
+      </View>
+    </View> */}
+
+    <View style={styles.container}>
+      <View style={styles.message}>
+        <Text>{text}</Text>
+        
+        <Text style={styles.time}>{time}</Text>
       </View>
     </View>
+    </>
   )
 }
 
 export default ChatMessage;
 
-/*
-COLOR SCHEMA
-radius buttons: #8757f7
-input- background: #8757f7
-output- background: #cabbed
-
-*/
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    margin: 10,
+  },
+  message: {
     borderRadius: 10,
-    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
     backgroundColor: '#cabbed',
     padding: 10,
     width: '50%',
-    marginLeft: 8
   },
+  time: {
+    textAlign: 'right',
+    paddingTop: 5,
+    paddingRight: 5,
+  }
+
   
 
 });
