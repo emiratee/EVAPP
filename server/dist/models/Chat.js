0b2b3f8dea6cc1455929ccba07cbf0dbfb50a0c2
erik <erik@lenzmann.net>
2023-12-04 12:08:17 +0100

    trigger

---
+++ server/dist/models/Chat.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const messageSchema = new mongoose_1.default.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    message: {
+        content: {
+            type: String,
+            required: true
+        },
+        time: {
+            type: String,
+            required: true
+        }
+    }
+});
+const chatSchema = new mongoose_1.default.Schema({
+    chatId: {
+        type: String,
+        required: true
+    },
+    participantIDs: [{
+            type: String
+        }],
+    chat: [{
+            type: messageSchema
+        }]
+});
+const Chat = mongoose_1.default.model('Chat', chatSchema);
+exports.default = Chat;
