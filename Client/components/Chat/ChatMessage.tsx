fcc1489bd3f0a2e0580073ef1fa5c917161b469d
erik <erik@lenzmann.net>
2023-12-05 11:30:18 +0100

    another one

--- Client/components/Chat/ChatMessage.tsx
+++ Client/components/Chat/ChatMessage.tsx
@@ -1,16 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 
 
 const ChatMessage = () => {
   return (
     <View>
-      <Text>ChatMessage</Text>
+      <Text style={{color: '#fff', textAlign: 'center', fontSize: 52}}>ChatMessage</Text>
+      <View style={styles.container}>
+        <Text>Hallihallo! wo bist du? Ich warte auf dich in Stresemannstr. 123C</Text>
+        <Text>time</Text>
+      </View>
     </View>
   )
 }
 
-export default ChatMessage
+export default ChatMessage;
 
-const styles = StyleSheet.create({})
+/*
+COLOR SCHEMA
+radius buttons: #8757f7
+input- background: #8757f7
+output- background: #cabbed
+
+*/
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+    borderBottomLeftRadius: 0,
+    backgroundColor: '#cabbed',
+    padding: 10,
+    width: '50%',
+    marginLeft: 8
+  },
+  
+
+});
