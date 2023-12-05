758e0c71c3650ddf851f087b1a76480a30dddc53
PilySwatch <pilar.cuellarta@gmail.com>
2023-12-05 13:02:43 +0100

    feat: remove swipe delete button from Message compònent & add it to MessageS tab - still only FE

--- Client/app/(tabs)/messages.tsx
+++ Client/app/(tabs)/messages.tsx
@@ -1,70 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigation } from 'expo-router';
 import { useAuth } from '../../utils/auth';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 import { getAllChats } from '../../utils/apiService';
 import { FlatList } from 'react-native-gesture-handler';
+import { Swipeable } from 'react-native-gesture-handler';
 import Message from '../../components/Message';
 
 export default function Messages() {
-    const { isAuthenticated, token } = useAuth();
-    const [chats, setChats] = useState([]);
-    const { navigate } = useNavigation(); 
+  const { isAuthenticated, token } = useAuth();
+  const [chats, setChats] = useState([]);
+  const { navigate } = useNavigation();
+  const [swipedMessageId, setSwipedMessageId] = useState(null);
+  const swipeableRef = useRef(null);
 
-    useEffect(() => {
-        if (!isAuthenticated) return navigate('login');
-        (async () => {
-            const fetchedChats = await getAllChats(token);            
-            setChats(fetchedChats.chats);
-        })();
-    }, [isAuthenticated, token]);
+  useEffect(() => {
+    if (!isAuthenticated) return navigate('login');
+    (async () => {
+      const fetchedChats = await getAllChats(token);
+      setChats(fetchedChats.chats);
+    })();
+  }, [isAuthenticated, token]);
 
+  const handleDelete = (messageId) => {
+    setChats((prevChats) => prevChats.filter((chat) => chat._id !== messageId));
+    setSwipedMessageId(null);
+  };
 
+  const renderSwipeDelete = (onPress) => (
+    <View style={{ width: 70, justifyContent: 'center', alignItems: 'center', backgroundColor: '#b0221d', borderRadius: 5, height: 60, marginTop: 15, marginRight: 10, }}>
+      <TouchableOpacity onPress={onPress}>
+        <Text style={{ color: 'white', fontSize: 16 }}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
-    const handleDelete = (messageId) => {
-        setChats((prevChats) => prevChats.filter((chat) => chat._id !== messageId));
-    };
+  const renderItem = ({ item }) => (
+    <Swipeable
+      renderRightActions={() => renderSwipeDelete(() => handleDelete(item._id))}
+      onSwipeableOpen={() => setSwipedMessageId(item._id)}
+      onSwipeableClose={() => setSwipedMessageId(null)}
+      ref={swipeableRef}
+    >
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => navigate('chatView', { chat: item })}
+        activeOpacity={1} // This makes sure that TouchableOpacity doesn't interfere with Swipeable
+      >
+        <Message item={item} />
+      </TouchableOpacity>
+    </Swipeable>
+  );
 
-
-    return (
-        <>
-            {chats.length > 0 && (
-                <FlatList
-                    data={chats}
-                    renderItem={({ item }) => {                        
-                        return (
-                            <TouchableOpacity style={styles.container} onPress={() => { return navigate('chatView', { chat: item }) }}>
-                                <Message item={item} onDelete={() => handleDelete(item._id)} />
-                            </TouchableOpacity>
-                        )
-                    }}
-                    keyExtractor={(item) => item._id}
-                />
-            )}
-        </>
-    );
+  return (
+    <>
+      {chats.length > 0 && (
+        <FlatList
+          data={chats}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id}
+        />
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    scrollContainer: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-    },
-    container: {
-        flex: 1,
-        width: '95%',
-        alignSelf: 'center',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        padding: 10,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0.5, height: 0 },
-        shadowOpacity: 0.4,
-        shadowRadius: 2,
-        marginTop: 10,
-    },
+  container: {
+    flex: 1,
+    width: '95%',
+    alignSelf: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0.5, height: 0 },
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    marginTop: 10,
+    marginBottom: 5,
+  },
 });
+
--- Client/components/Message.tsx
+++ Client/components/Message.tsx
@@ -1,73 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { useAuth } from '../utils/auth';
 import * as icons from '@expo/vector-icons';
-import { Swipeable } from 'react-native-gesture-handler';
 
+const Message = ({ item }) => {
+  const { user } = useAuth();
+  const name = item.driver.userId === user.userId ? item.passenger.name : item.driver.name;
+  const imageUrl = item.driver.userId === user.userId ? item.passenger.imageUrl : item.driver.imageUrl;
+  const lastMessage = item.chat.length > 0 && item.chat[item.chat.length - 1].message.content;
 
-const Message = ({ item, onDelete }) => {
-    const { user } = useAuth();   
-    const name = item.driver.userId === user.userId ? item.passenger.name : item.driver.name;
-    const imageUrl = item.driver.userId === user.userId ? item.passenger.imageUrl : item.driver.imageUrl;
-    const lastMessage = item.chat.length > 0 && item.chat[item.chat.length - 1].message.content;
-
-    
-    const renderSwipeDelete = () => {
-        return (
-          <View style={{ width: 70, justifyContent: 'center', alignItems: 'center', backgroundColor: '#b0221d', borderRadius: 5, height: 40, marginTop: 5, }}>
-            <Text style={{ color: 'white' }}>Delete</Text>
+  return (
+    <View style={[styles.message, { justifyContent: 'space-between' }]}>
+      <View style={styles.message}>
+        {imageUrl ? (
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+        ) : (
+          <icons.AntDesign name="user" size={40} color="black" />
+        )}
+        {lastMessage ? (
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: '600' }}>{name}</Text>
+            <Text style={{ fontSize: 12, fontWeight: '300', fontStyle: 'italic' }}>
+              {lastMessage.length >= 35 ? lastMessage.substring(0, 32) + '...' : lastMessage}
+            </Text>
           </View>
-        );
-    };
-    
-    return (
-        <Swipeable
-          renderRightActions={renderSwipeDelete}
-          onSwipeableOpen={() => onDelete(item.id)}
-        >
-            <View style={[styles.message, { justifyContent: 'space-between' }]}>
-                <View style={styles.message}>
-                {imageUrl ? (
-                    <Image source={{ uri: imageUrl }} style={styles.image} />
-                ) : (
-                    <icons.AntDesign name="user" size={40} color="black" />
-                )}
-                {lastMessage ? (
-                    <View>
-                    <Text style={{ fontSize: 20, fontWeight: '600' }}>{name}</Text>
-                    <Text style={{ fontSize: 12, fontWeight: '300', fontStyle: 'italic' }}>
-                        {lastMessage.length >= 35 ? lastMessage.substring(0, 32) + '...' : lastMessage}
-                    </Text>
-                    </View>
-                ) : (
-                    <View>
-                    <Text style={{ fontSize: 20, fontWeight: '600' }}>{name}</Text>
-                    </View>
-                )}
-                </View>
-        
-            </View>
-        </Swipeable>
-    );
+        ) : (
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: '600' }}>{name}</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  message: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 15,
+  },
+  image: {
+    height: 50,
+    width: 50,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: '#000',
+  },
+});
 
 export default Message;
 
-const styles = StyleSheet.create({
-    message: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        gap: 15,
-    },
-    image: {
-        height: 50,
-        width: 50,
-        padding: 8,
-        borderWidth: 1,
-        borderRadius: 50,
-        borderColor: '#000',
-    },
-
-});
 
