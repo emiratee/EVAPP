20882ce2ed1ebdebb23001e8e2c737a9303e97a7
erik <erik@lenzmann.net>
2023-12-05 18:38:20 +0100

    first test

--- server/routers/socketRouter.ts
+++ server/routers/socketRouter.ts
@@ -1,12 +1,16 @@
 import { Server, Socket } from 'socket.io';
+import { getUserById } from '../controllers/chatController';
+import { sendPushNotification } from '../utils/userUtils';
 
 const socketRouter = (io: Server) => {
     io.on('connection', (socket: Socket) => {
         socket.on('conversation', (chatId: string) => {
             socket.join(chatId);
             
-            socket.on('message', (content: any, receiverId: string) => {
+            socket.on('message', async (content: any, receiverId: string) => {
                 socket.in(chatId).to(receiverId).emit('message', content);
+                const receiverUser = await getUserById(receiverId);
+                receiverUser.expoPushToken && await sendPushNotification(receiverUser.expoPushToken, 'New message', 'Test');
             });
             
             socket.on('disconnect', () => {
