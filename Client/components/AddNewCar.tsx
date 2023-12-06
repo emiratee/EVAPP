fbfef96f5a8178b6ac6f9fb9667379a2fffa4220
Aaisha Rehman <aashu1471@gmail.com>
2023-12-06 15:59:15 +0100

    sync

--- Client/components/AddNewCar.tsx
+++ Client/components/AddNewCar.tsx
@@ -22,81 +22,81 @@ const AddNewCar = (props: Props) => {
     return (
         user && token && <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
             <View style={{ width: '90%' }}>
-                <Text style={{ fontSize: 24, textAlign: 'center' }}>Add New Car</Text>
+                <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold' }}>Add New Car</Text>
 
 
-                <View style={styles.parameters}>
-                    <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
-                        <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
-                            <icons.FontAwesome5 name='car' size={24} color={'black'} />
-                            <Text>Model: </Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            editable
-                            onChangeText={text => setNewModel(text)}
-                            value={newModel}
-                            placeholder="Type here..."
-                            placeholderTextColor="#838383"
-                            testID="model-input"
+            <View style={styles.parameters}>
+                <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
+                    <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
+                        <icons.FontAwesome5 name='car' size={24} color={COLORS.iconColor} />
+                        <Text>Model: </Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        editable
+                        onChangeText={text => setNewModel(text)}
+                        value={newModel}
+                        placeholder="Type here..."
+                        placeholderTextColor="#838383"
+                        testID="model-input" 
 
-                        />
+                    />
+                </View>
+                <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
+                    <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
+                        <icons.Ionicons name='color-fill' size={24} color={COLORS.iconColor} />
+                        <Text>Color: </Text>
                     </View>
-                    <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
-                        <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
-                            <icons.Ionicons name='color-fill' size={24} color={'black'} />
-                            <Text>Color: </Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            editable
-                            onChangeText={text => setNewColor(text)}
-                            value={newColor}
-                            placeholder="Type here..."
-                            placeholderTextColor="#838383"
-                            testID="color-input"
-                        />
+                    <TextInput
+                        style={styles.input}
+                        editable
+                        onChangeText={text => setNewColor(text)}
+                        value={newColor}
+                        placeholder="Type here..."
+                        placeholderTextColor="#838383"
+                        testID="color-input" 
+                    />
+                </View>
+                <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
+                    <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
+                        <icons.Ionicons name='documents' size={24} color={COLORS.iconColor} />
+                        <Text>Licence Plates: </Text>
                     </View>
-                    <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
-                        <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
-                            <icons.Ionicons name='documents' size={24} color={'black'} />
-                            <Text>Licence Plates: </Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            editable
-                            onChangeText={text => setNewLicencePlates(text)}
-                            value={newLicencePlates}
-                            placeholder="Type here..."
-                            placeholderTextColor="#838383"
-                            testID="licence-plates-input"
-                        />
-                    </View>
+                    <TextInput
+                        style={styles.input}
+                        editable
+                        onChangeText={text => setNewLicencePlates(text)}
+                        value={newLicencePlates}
+                        placeholder="Type here..."
+                        placeholderTextColor="#838383"
+                        testID="licence-plates-input"
+                    />
+                </View>
 
-                    <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
-                        <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
-                            <icons.Ionicons name="ios-people" size={24} color={'black'} />
-                            <Text>Number of seats: </Text>
-                        </View>
-                        <RNPickerSelect
-                            onValueChange={(value) => { setNewNumberOfSeats(value) }}
-                            style={{
-                                inputIOS: styles.input,
-                                inputAndroid: styles.input,
-                            }}
-                            placeholder={{
-                                label: '1',
-                                value: 1,
-                            }}
-                            items={[
-                                { label: '2', value: 2 },
-                                { label: '3', value: 3 },
-                                { label: '4', value: 4 },
-                                { label: '5', value: 5 },
-                                { label: '6', value: 6 },
-                            ]}
-                        />
+                <View style={[styles.parameter, { flexDirection: 'column', gap: 10 }]}>
+                    <View style={[styles.iconContainer, { alignSelf: 'flex-start' }]}>
+                        <icons.Ionicons name="ios-people" size={24} color={COLORS.iconColor} />
+                        <Text>Number of seats: </Text>
                     </View>
+                    <RNPickerSelect
+                        onValueChange={(value) => { setNewNumberOfSeats(value) }}
+                        style={{
+                            inputIOS: styles.input,
+                            inputAndroid: styles.input,
+                        }}
+                        placeholder={{
+                            label: '1',
+                            value: 1,
+                        }}
+                        items={[
+                            { label: '2', value: 2 },
+                            { label: '3', value: 3 },
+                            { label: '4', value: 4 },
+                            { label: '5', value: 5 },
+                            { label: '6', value: 6 },
+                        ]}
+                    />
+                </View>
 
                     <TouchableOpacity
                         style={styles.btn}
@@ -122,7 +122,7 @@ const AddNewCar = (props: Props) => {
 
                         }}
                     >
-                        <Text >Add a car</Text>
+                        <Text style={{color: '#fff', fontSize: 16}}>Add a car</Text>
                     </TouchableOpacity>
                 </View>
             </View >
@@ -137,10 +137,11 @@ const styles = StyleSheet.create({
     input: {
         height: 50,
         width: '100%',
-        borderWidth: 1,
+        // borderWidth: 1,
         padding: 10,
         borderRadius: 10,
         borderColor: '#a8a8a8',
+        backgroundColor: COLORS.inputFields,
     },
     iconContainer: {
         flexDirection: 'row',
@@ -163,9 +164,14 @@ const styles = StyleSheet.create({
         width: windowWidth * 0.8
     },
     btn: {
-        backgroundColor: COLORS.textColour,
+        backgroundColor: COLORS.buttonBackground,
         alignItems: 'center',
         padding: 20,
-        borderRadius: 10
+        marginTop: 10,
+        borderRadius: 25,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2
     },
 })
